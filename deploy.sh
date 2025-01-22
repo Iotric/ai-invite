0f138c3eb143d4ff384c75f4feb8a44f4de29268
akash-127 <143987300+akash-127@users.noreply.github.com>
2025-01-22 13:12:02 +0530

    text-processing-deployment-configuration-added

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# initial setup needed before it
+# aws configuration
+# docker desktop or daemon running
+ACCOUNT_ID  = ""
+IMAGE_NAME  = "noun_pronoun"
+# build docker image at local
+docker build -t ${IMAGE_NAME}  .
+# docker cli to aws ecr registry
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com
+# create ECR repository  || if repository already exist skip it
+aws ecr create-repository --repository-name ${IMAGE_NAME} --region us-east-1 --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
+# create docker image  same as name of reposity
+docker tag ${IMAGE_NAME} ${ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${IMAGE_NAME}:latest
+# push the local image to the aws ecr repository
+docker push ${ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${IMAGE_NAME}:latest
